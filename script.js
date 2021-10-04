30f0739df00ed71f3435bd19c1e7b3a2e4fd4f8a
Alexander Panasik <rastpk@gmail.com>
2021-10-04 21:37:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const numberOfFilms = +prompt("How many films have you already watched?", "");
+
+// console.log(numberOfFilms);
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres: [],
+    privat : true
+};
+
+const a = prompt("Name last movie", ""),
+      b = prompt("Name your mark", ""),
+      c = prompt("Name last movie", ""),
+      d = prompt("Name your mark", "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
